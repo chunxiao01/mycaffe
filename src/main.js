c4265a5bb0e69f88c71ad1732bf75cf2233d50ee
chunxiao01 <12861187+chunxiao01@users.noreply.github.com>
2022-09-21 17:16:15 +0800

    feat:新增Toast全局插件

--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import App from "./App.vue"
 
 import VueLazyload from "vue-lazyload"
 
+import Toast from "./components/common/toast/index.js"
+
 import router from "./router"
 import store from "./store"
 
 Vue.config.productionTip = false
 
 Vue.use(VueLazyload)
+Vue.use(Toast) //全局注册Toast
 
 new Vue({
   router,
